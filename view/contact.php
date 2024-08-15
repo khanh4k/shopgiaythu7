1d73f04cb7c63ea79c1a6d8ac4d339567126aac3
khanh4k <tranvankhanh2k4@gmail.com>
2024-08-15 11:00:33 +0700

    Add files via upload

---
+++ view/contact.php
@@ -0,0 +1,85 @@
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="bread"><span><a href="index.html">Trang Chủ</a></span> / <span>Liên Hệ</span></p>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="colorlib-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<h3>Thông tin liên lạc</h3>
+						<div class="row contact-info-wrap">
+							<div class="col-md-3">
+								<p><span><i class="fas fa-location-dot"></i></span> Q4,<br> HCM, Vietnam</p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="fas fa-phone"></i></span> <a href="tel://0931294567">0931294567</a></p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="fas fa-paper-plane"></i></span> <a href="mailto:info@yoursite.com">TKKshop.com.vn</a></p>
+							</div>
+							<div class="col-md-3">
+								<p><span><i class="fas fa-globe"></i></span> <a href="#">TKKshop@gmail.com</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="contact-wrap">
+							<h3>Liên lạc</h3>
+							<form action="#" class="contact-form">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="fname">Họ</label>
+											<input type="text" id="fname" class="form-control" placeholder="Họ">
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="lname">Tên</label>
+											<input type="text" id="lname" class="form-control" placeholder="Tên">
+										</div>
+									</div>
+									<div class="w-100"></div>
+									<div class="col-sm-12">
+										<div class="form-group">
+											<label for="email">Email</label>
+											<input type="text" id="email" class="form-control" placeholder="Email">
+										</div>
+									</div>
+									<div class="w-100"></div>
+									<div class="col-sm-12">
+										<div class="form-group">
+											<label for="subject">Tiêu đề</label>
+											<input type="text" id="subject" class="form-control" placeholder="Tiêu đề">
+										</div>
+									</div>
+									<div class="w-100"></div>
+									<div class="col-sm-12">
+										<div class="form-group">
+											<label for="message">Nội dung</label>
+											<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Nội dung"></textarea>
+										</div>
+									</div>
+									<div class="w-100"></div>
+									<div class="col-sm-12">
+										<div class="form-group">
+											<input type="submit" value="Gửi" class="btn btn-primary">
+										</div>
+									</div>
+								</div>
+							</form>		
+						</div>
+					</div>
+					<div class="col-md-6">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.467629182789!2d106.70130297428763!3d10.775451359213278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4802af4703%3A0x958af8c6972ad52c!2sSheraton%20Saigon%20Grand%20Opera%20Hotel!5e0!3m2!1sen!2s!4v1723692160650!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>	
+					</div>
+				</div>
+			</div>
+		</div>
